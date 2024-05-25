2c421d16d34719773492aee315742b78d6616308
Jake Edwards <jake@xvii.com.au>
2024-05-25 11:20:41 +1000

    format cleanup

--- assets/background.js
+++ assets/background.js
@@ -3,20 +3,21 @@ var client = ZAFClient.init()
 client.on('app.registered', function (context) {
   var settings = context.metadata.settings
 
-  client.get('instances').then(function(data) {
+  client.get('instances').then(function (data) {
     var instanceId
 
-    Object.keys(data.instances).forEach(function(key) {
+    Object.keys(data.instances).forEach(function (key) {
       if (data.instances[key].location === 'ticket_sidebar') {
         instanceId = key
       }
     })
-	
+
     setIframeHeight(client.instance(instanceId), settings)
-	 
+
   })
 })
 
 function setIframeHeight(client, settings) {
-	client.invoke('resize', { width: '100%', height: settings.iframeHeight });
+  console.log("iframe resizing")
+  client.invoke('resize', { width: '100%', height: settings.iframeHeight });
 }
--- assets/iframe.js
+++ assets/iframe.js
@@ -1,31 +1,32 @@
 var client = ZAFClient.init()
 
-client.on('app.registered', function(context) {
+client.on('app.registered', function (context) {
   var settings = context.metadata.settings
+  console.log("iframe app.registered")
   window.location.replace((settings.zafSdkSupport) ? buildUrl(window.location.href, settings.iframeURL) : settings.iframeURL)
-})
-
-function buildUrl (currentUrl, newUrl) {
-  if (!/^https?:/.test(newUrl)) {
-    newUrl = 'http://' + newUrl
-  }
-  var settingsUrl = urlParser(newUrl)
-  var newParams = [].concat(queryParameters(currentUrl), queryParameters(newUrl)).join('&')
-  var insertIEslash = settingsUrl.pathname.indexOf('/') === 0 ? '' : '/' // IE doesn't start pathname with a slash
-  return settingsUrl.protocol + '//' + settingsUrl.host + insertIEslash + settingsUrl.pathname + '?' + newParams + settingsUrl.hash
 }
 
-function urlParser (url) {
-  var $a = document.createElement('a')
-  $a.href = url
+function buildUrl(currentUrl, newUrl) {
+    if (!/^https?:/.test(newUrl)) {
+      newUrl = 'http://' + newUrl
+    }
+    var settingsUrl = urlParser(newUrl)
+    var newParams = [].concat(queryParameters(currentUrl), queryParameters(newUrl)).join('&')
+    var insertIEslash = settingsUrl.pathname.indexOf('/') === 0 ? '' : '/' // IE doesn't start pathname with a slash
+    return settingsUrl.protocol + '//' + settingsUrl.host + insertIEslash + settingsUrl.pathname + '?' + newParams + settingsUrl.hash
+}
 
-  // IE 9, 10, 11 CRUFT: when the url is relative the protocol is ':', so we force it to be absolute
-  $a.href = $a.href
-  return $a
+function urlParser(url) {
+    var $a = document.createElement('a')
+    $a.href = url
+
+    // IE 9, 10, 11 CRUFT: when the url is relative the protocol is ':', so we force it to be absolute
+    $a.href = $a.href
+    return $a
 }
 
 // returns array of queryString Params ['zat=true', 'zaf=local']
-function queryParameters (url) {
-  // remove inital ? and split
-  return urlParser(url).search.replace(/^\?/, '').split('&').filter(function(p) { return !!p })
+function queryParameters(url) {
+    // remove inital ? and split
+    return urlParser(url).search.replace(/^\?/, '').split('&').filter(function (p) { return !!p })
 }
